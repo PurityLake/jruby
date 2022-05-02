729d5a465ff118187e7b848784563bd92841f387
PurityLake <robertoshea2k11@gmail.com>
2022-05-02 22:19:01 +0100

    Partial fix for sort! freeze test

--- test/mri/ruby/test_array.rb
+++ test/mri/ruby/test_array.rb
@@ -1695,7 +1695,7 @@ class TestArray < Test::Unit::TestCase
     assert_raise(FrozenError) do
       count = 0
       array.sort! do |a, b|
-        array.freeze if (count += 1) == 6
+        array.freeze if (count += 1) == 4
         frozen_array ||= array.map.to_a if array.frozen?
         b <=> a
       end
@@ -1714,7 +1714,7 @@ class TestArray < Test::Unit::TestCase
     object = Object.new
     array = [object, object]
     object.define_singleton_method(:>){|_| array.freeze; true}
-    object.define_singleton_method(:<=>){|o| object}
+    object.define_singleton_method(:<=>){|o| array.freeze; o}
     assert_raise(FrozenError) do
       array.sort!
     end
